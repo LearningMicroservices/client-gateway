19e773a6c6682dbcc90412e447fed1c0ce8e38eb
Juan José Medina <jjr20060326@gmail.com>
2025-03-09 17:19:41 -0500

    Add authentication module with user registration and login functionality

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
+
+import { catchError } from 'rxjs';
+import { LoginUserDto } from './dto/login-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { AuthGuard } from './guards/auth.guard';
+import { Token } from './decorators/token.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Post('register')
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('verify')
+  verifyToken(@Token() token: string) {
+    // const user = req['user'];
+    // const token = req['token'];
+    return { token };
+    // return { user, token };
+  }
+}
